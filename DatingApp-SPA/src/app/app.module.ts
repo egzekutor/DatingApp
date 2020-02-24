dba4ea72cffdec909bfe35c554a421a27a331952
Rafał Chra <spacerafal@gmail.com>
2020-02-24 16:04:13 +0100

    routes

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -11,15 +11,29 @@ import { AuthService } from "./_services/auth.service";
 import { HomeComponent } from "./home/home.component";
 import { RegisterComponent } from "./register/register.component";
 import { ErrorInterceptorProvider } from "./_services/error.interceptor";
+import { ListsComponent } from "./lists/lists.component";
+import { MemberListComponent } from "./member-list/member-list.component";
+import { MessagesComponent } from "./messages/messages.component";
+import { RouterModule } from "@angular/router";
+import { appRoutes } from "./routes";
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
+  declarations: [
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    RegisterComponent,
+    ListsComponent,
+    MemberListComponent,
+    MessagesComponent
+  ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
     BrowserAnimationsModule,
-    BsDropdownModule.forRoot()
+    BsDropdownModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [AuthService, ErrorInterceptorProvider],
   bootstrap: [AppComponent]
